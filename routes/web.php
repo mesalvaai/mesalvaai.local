c377e988e2ac9c03bfd5f894ff410e0c5c4deac9
Eber Ortiz <ortizmas14@gmail.com>
2018-06-11 14:24:56 -0300

    Atualizado para versão 5.6 e limpando conteudo

--- routes/web.php
+++ routes/web.php
@@ -18,46 +18,14 @@
 Route::get('/', 'HomeController@home');
 
 Route::get('/cursos', 'CursoController@curso');
-
 Route::get('/faculdade/id_curso', 'CursoController@faculdade');
-
-Route::get('/lista', 'FrutasController@getSaveFruta');
 
 Route::get('/teste', function () {
     return view('teste');
 });
 
-Route::get('/hola-mundo', function () {
-    return 'Hola sou desde o route';
-});
 
-Route::post('/hola-mundo', function () {
-    return 'Hola sou desde o route';
-});
 
-Route::get('/contato/{nombre?}/{edad?}', function ($nombre = "Eber Ortiz", $edad=20) {
-    return view('contato', array(
-    	"nombre" => $nombre,
-    	"edad" => $edad
-    ));
-})->where([
-	'nombre' => '[A-Za-z]+',
-	'edad' => '[0-9]+'
-]);
-
-Route::get('/url_post', 'TestController@index');
-
-// Route::get('/fruta', 'FrutasController@index');
-// Route::get('/naranja', 'FrutasController@naranja');
-// Route::get('/pera', 'FrutasController@pera');
-
-Route::post('/receber', 'FrutasController@receberForm');
-
-Route::group(['prefix' => 'frutaria'], function(){
-    Route::get('/fruta', 'FrutasController@index');
-    Route::get('/naranja/{admin?}', ['middleware' => 'EsAdmin', 'uses' => 'FrutasController@naranja', 'as' => 'naranjitas']);
-    Route::get('/pera', 'FrutasController@pera');
-});
 
 
 
