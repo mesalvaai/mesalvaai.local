cc8415a1df0a3ab9a1b2134791d341f276fa8ec7
Eber Ortiz <ortizmas14@gmail.com>
2018-06-11 14:52:47 -0300

    Instalando o pacote laravel collective

--- config/app.php
+++ config/app.php
@@ -177,6 +177,10 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        //Laravel collective
+        Collective\Html\HtmlServiceProvider::class,
+
+
     ],
 
     /*
@@ -225,6 +229,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
 
     ],
 
