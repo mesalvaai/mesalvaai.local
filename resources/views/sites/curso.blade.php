a1ffb214ef43dac9a6add0256df8f338e4071298
Bianca <Bianca@DESKTOP-8387F1A>
2018-07-25 21:44:34 -0300

    Alteração Layout Cursos

--- resources/views/sites/curso.blade.php
+++ resources/views/sites/curso.blade.php
@@ -1,13 +1,7 @@
 @extends('layouts.site.app', ['title' => 'ME SALVA AI'])
 
-@section('header')
-	@include('layouts.site.partials.menu', ['some' => 'data'])
-@endsection
 
 @section('content')
 	@include('sites.cursos.index')
 @endsection
 
-@section('footer')
-	@include('layouts.site.partials.footer', ['some' => 'data'])
-@endsection
