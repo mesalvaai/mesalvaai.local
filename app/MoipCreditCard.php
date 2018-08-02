f5f261865faba642760daf25a3608947fce4a505
wallas123 <wallas.rocha-castro@hotmail.com>
2018-08-01 21:36:32 -0300

    Tirando comentarios dos dados do cartão criptografados

--- app/MoipCreditCard.php
+++ app/MoipCreditCard.php
@@ -13,7 +13,8 @@ class MoipCreditCard extends Model
 	{
 
 
-$hash = base64_decode($hash);
+		$hash = base64_decode($hash);
+
 
 		$moip = Moip::start();
 
@@ -24,13 +25,11 @@ $hash = base64_decode($hash);
 			->setBirthDate('1988-12-30')
 			->setTaxDocument('22222222222')
 			->setPhone(11, 66778899)
-			// ->setCreditCard(12, 21, '4073020000000002', '123')
 			->addAddress('SHIPPING',
 				'Rua de teste do SHIPPING', 123,
 				'Bairro do SHIPPING', 'Sao Paulo', 'SP',
 				'01234567', 8)
 			->create();
-			// dd($customer);
 		} catch (Exception $e) {
 			dd($e->__toString());
 		}
@@ -40,37 +39,27 @@ $hash = base64_decode($hash);
 
 		try {
 
-//set OwnId único, adiciona item [doação, a quantidade, detalhe, e valor no ex. 100 R$]
-
+            //set OwnId único, adiciona item [doação, a quantidade, detalhe, e valor no ex. 100 R$]
+			
 			$order = $moip->orders()->setOwnId(uniqid())
 			->addItem("Doação",1, "sku1", 10000)
 			->setCustomer($customer)
 			->create();
-
-			//dd($order);
 		} catch (Exception $e) {
 			dd($e->__toString());
 		}
 
 
+		try {
+
+			$payment = $order->payments()->setCreditCardHash($hash, $customer)
+			->execute();
+			return "ok";
+
+		} catch (Exception $e) {
+			dd($e->__toString());
+		}
 		
-		// try {
-		// 	$payment = $order->payments()->setCreditCard(12, 21, '4073020000000002', '123', $customer)
-		// 	 ->execute();
-
-
-		//  	// dd($payment);
-		// } catch (Exception $e) {
-		// 	dd($e->__toString());
-		// }
-
-		$hash = "aOC1jHRnujnHB2Jgx7rslF3MKkS62pZH0DXhkDgSWYKexahKoE/2adVpye4YldSQK/O/0pFtLoU1ZuLBxnE5LWvd/3Lkh9kVSlNjS3o2WTb93rTk5jWk9aVIr0WaAFNEswOIqIVp676e1J+Lyd8kHBXVheeAegimy08iP1g1+YiamfbEcmWVZSj4AZBvoIMCqyBtESMCMmw49k24ty7Bx8YcT5YCljEk2+Mvs6Z+Rj1btxzudGr9xVBoKl/zTpz9lx8PMlo8vaLxX0+IISaHOKaEkYwlyt0fcIcou3mMu5/Xfq72uHdeT+KiNefIu81YpsBhrnG4uh0NamXZya2gqg==";
-		$payment = $order->payments()->setCreditCardHash($hash, $customer)
-		// ->setInstallmentCount(3)
-		// ->setStatementDescriptor('teste de pag')
-		  ->execute();
-		//dd($payment);
-		 return "ok";
 	}
 
 
