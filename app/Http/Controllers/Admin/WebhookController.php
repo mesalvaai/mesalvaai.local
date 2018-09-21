cffb326efb3f593f212f0a88dbb67d513ad29acb
Eber Ortiz <ortizmas14@gmail.com>
2018-09-21 15:16:38 -0300

    WebHooks

--- app/Http/Controllers/Admin/WebhookController.php
+++ app/Http/Controllers/Admin/WebhookController.php
@@ -5,6 +5,11 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Response;
+use Moip;
+
 class WebhookController extends Controller
 {
     /**
@@ -14,7 +19,40 @@ class WebhookController extends Controller
      */
     public function index(Request $request)
     {
-        dd($request);
+        $moip = Moip::start();
+        // To list all Webhooks
+        // Recibir el cuerpo de la petición.
+        $input = @file_get_contents("php://input");
+        // Parsear el contenido como JSON.
+        $eventJson = json_decode($input);
+
+        // Usar los datos del Webhooks para alguna acción.
+        dd($eventJson);
+        // Responder
+        http_response_code(200);
+        
+        // $response = \Response::json('POST', '/info', ['name' => 'Sally']);
+
+        // $response
+        //     ->assertStatus(201)
+        //     ->assertJson([
+        //         'created' => true,
+        //     ]);
+
+
+
+        // To list Webhooks, using pagination and filter
+        //$moip->webhooks()->get(new Pagination(10, 0), "ORD_ID", "ORDER.PAID");
+                // try {
+        //     // $notification = $moip->notifications()->addEvent('ORDER.*')
+        //     //         ->addEvent('PAYMENT.AUTHORIZED')
+        //     //         ->setTarget('http://requestb.in/1dhjesw1')
+        //     //         ->create();
+        //     $notification = $moip->notifications()->get('NPR-DV61EEGGUFCQ');
+        //     dd($notification);
+        // } catch (Exception $e) {
+        //     printf($e->__toString());    
+        // }
     }
 
     /**
